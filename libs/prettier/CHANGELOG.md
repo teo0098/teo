54e8b0c8671273cf8e2d242cf83f9f07853d2e18
Teodor Tkaczyk <teodor.tkaczyk0407@gmail.com>
2022-08-24 20:19:08 +0200

    build: setup nx monorepo

---
+++ libs/prettier/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @teo0098/prettier
+
+## 0.0.1
+
+### Patch Changes
+
+- Add prettier configuration options
---
+++ libs/prettier/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+  jsxBracketSameLine: false,
+  printWidth: 80,
+  proseWrap: 'preserve',
+  requirePragma: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+};
