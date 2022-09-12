9b4662ee1d26f346843fb21c9a0ea0576719bcf4
Teodor Tkaczyk <teodor.tkaczyk0407@gmail.com>
2022-09-12 21:53:20 +0200

    chore(packages): install eslint and typescript newer versions and add prebuild script npm install

--- libs/ui/.eslintrc.js
+++ libs/ui/.eslintrc.js
@@ -5,14 +5,4 @@ module.exports = {
     tsconfigRootDir: __dirname,
   },
   ignorePatterns: ['.eslint*'],
-  rules: {
-    'import/prefer-default-export': 'off',
-    'react/function-component-definition': [
-      2,
-      {
-        namedComponents: ['arrow-function', 'function-declaration'],
-        unnamedComponents: 'arrow-function',
-      },
-    ],
-  },
 };
