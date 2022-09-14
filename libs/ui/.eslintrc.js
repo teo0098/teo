f622755b6f299c62bb93385cb336bec94eea51c0
Teodor Tkaczyk <teodor.tkaczyk0407@gmail.com>
2022-09-14 22:55:15 +0200

    refactor(ui package): use relative path when extending eslint-config pacakge

--- libs/ui/.eslintrc.js
+++ libs/ui/.eslintrc.js
@@ -1,18 +1,8 @@
 module.exports = {
   root: true,
-  extends: '@teo0098/eslint-config',
+  extends: '../eslint-config',
   parserOptions: {
     tsconfigRootDir: __dirname,
-  },
-  rules: {
-    'import/prefer-default-export': 'off',
-    'react/function-component-definition': [
-      2,
-      {
-        namedComponents: ['arrow-function', 'function-declaration'],
-        unnamedComponents: 'arrow-function',
-      },
-    ],
   },
   ignorePatterns: ['.eslint*', 'dist'],
 };
